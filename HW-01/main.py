faab8177cc2971b1f428d3e4a2a3de16e0471247
Yves Velasquez <vyves@csu.fullerton.edu>
2026-02-05 16:26:17 -0800

    setup

---
+++ HW-01/main.py
@@ -0,0 +1,21 @@
+"""
+User Input:
+o Prompt the user to enter a line of text.
+• Write to File
+o Store the entered text in a file on disk.
+o If the file does not exist, create it.
+o If the file already exists, overwrite its contents.
+• Read from File
+
+Open the same file.
+o Read the stored line of text from the file.
+• Output
+o Display:
+ the text read from the file
+ the file path where the text was stored
+• Error Handling:
+o Your program must handle and report errors for:
+ Failure to create or open the file
+ Failure to write to the file
+ Failure to read from the file
+"""
