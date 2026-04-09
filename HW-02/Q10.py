40a67253499f1d9a4f65f79f28d968c516323fc7
Yves <vyves246@gmail.com>
2026-04-09 14:34:03 -0700

    idk

--- HW-02/Q10.py
+++ HW-02/Q10.py
@@ -7,20 +7,20 @@ def createSequences(num_requests,cylinders):
 
 
 # Accept the initial head position as input.
+total_cylinders = int(input("Enter total number of cylinders: "))
+requests = int(input("Enter number of requests: "))
 initial_head_position = int(input("Enter initial head position: "))
 
-total_cylinders = int(input("Enter total number of cylinders: "))
-requests = int(input("Please enter # of requests: "))
 disk_request = createSequences(num_requests=requests, cylinders=total_cylinders)
-
-print(disk_request)
+print(f"Generated Requests: {disk_request}")
 
 total_head_movement = 0
 current_position = initial_head_position
 
 for index in range(len(disk_request)):
     distance = abs(current_position - disk_request[index])
-    print(f"Distance from {current_position} and {disk_request[index]}: {distance}")
+    #print(f"Distance from {current_position} and {disk_request[index]}: {distance}")
+    #print(f"{current_position} -> {disk_request[index]} ", end="")
     total_head_movement += distance
     current_position = disk_request[index]
-print(f"Total Head Movement: {total_head_movement}")
+print(f" \n Total Head Movement: {total_head_movement}")
